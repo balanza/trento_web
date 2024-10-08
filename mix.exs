c8b833a2dad7d004384436add69176653616b313
balanza <ema.decu@gmail.com>
2024-10-08 16:03:49 +0200

    typo

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Trento.MixProject do
   def project do
     [
       app: :trento,
-      description: "Easing your life in administering SAP applications",
+      description: "Easing TYPO your life in administering SAP applications",
       version: get_version(),
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
